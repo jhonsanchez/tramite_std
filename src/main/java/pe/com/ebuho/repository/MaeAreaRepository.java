03701399a23e6bc6978ede193f0757315658f7fc
jhonmontalvan <jhonsanchez@ebuho.com>
2015-11-02 18:12:22 -0500

    Se agrego lista de archivos ignorados en GIT y se modifico el repositorio

---
+++ src/main/java/pe/com/ebuho/repository/MaeAreaRepository.java
@@ -0,0 +1,28 @@
+package pe.com.ebuho.repository;/**
+ * <pre>
+ * MaeAreaRepository.java
+ * Copyright (c) 2015
+ * Creado el: 02/11/2015
+ * Propósito:
+ * </pre>
+ * @author: eBuho
+ * @version 01.00.00
+ */
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.com.ebuho.model.MaeArea;
+
+/**<pre>
+ * Versión  |    Fecha    |   Modificado por   |  Método Modificado |  Desc. del cambio
+ *-----------------------------------------------------------------------------------------------------------------
+ *  01.00    02/11/2015       eBuho        Ninguno.	              Se creó la clase.
+ *  01.01     
+ * </pre>
+ */
+
+public interface MaeAreaRepository extends JpaRepository<MaeArea,BigDecimal> {
+    List<MaeArea> findByDescAreaAre(String descAreaAre);
+}
