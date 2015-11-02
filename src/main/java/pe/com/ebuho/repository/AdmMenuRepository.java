9471d1944076e5f9c2954d83b0ca63b108bf41a6
jhonmontalvan <jhonsanchez@ebuho.com>
2015-11-02 17:36:23 -0500

    Se agrego lista de archivos ignorados en GIT y se modifico el repositorio

--- src/main/java/pe/com/ebuho/repository/AdmMenuRepository.java
+++ src/main/java/pe/com/ebuho/repository/AdmMenuRepository.java
@@ -1,5 +1,6 @@
 package pe.com.ebuho.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import pe.com.ebuho.model.AdmMenu;
 
@@ -8,5 +9,5 @@ import java.math.BigDecimal;
 /**
  * Created by John on 10/29/2015.
  */
-public interface AdmMenuRepository extends CrudRepository<AdmMenu,BigDecimal> {
+public interface AdmMenuRepository extends JpaRepository<AdmMenu,BigDecimal> {
 }
