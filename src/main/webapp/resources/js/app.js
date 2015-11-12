367b4200c44f8206499f974ad8b5e79f9bb8c9bf
jhonmontalvan <jhonsanchez@ebuho.com>
2015-11-12 18:03:31 -0500

    Pure angularJS

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -12,5 +12,5 @@ app.controller('listdata', function ($scope, $http) {
 (function(angular) {
     angular.module("myApp.controllers", []);
     angular.module("myApp.services", []);
-    angular.module("myApp", ["ngResource", "spring-data-rest", "myApp.controllers", "myApp.services"]);
+    angular.module("myApp", ["ngResource", "spring-data-rest", "myApp.controllers", "myApp.services","ui.bootstrap"]);
 }(angular));
